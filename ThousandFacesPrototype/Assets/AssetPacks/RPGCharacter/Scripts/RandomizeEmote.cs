06e80021845c4d156856616c6a460d917ee5d1c1
Tristan Bowler <tristan.bowler@gmail.com>
2020-08-30 11:28:51 -0600

    commit 9

---
+++ ThousandFacesPrototype/Assets/AssetPacks/RPGCharacter/Scripts/RandomizeEmote.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeEmote : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string[] bools;
+    public string current;
+
+    void Start()
+    {
+        int rand = Random.Range(0, bools.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator WaitFunc()
+    {
+        yield return new WaitForSeconds(5);
+       // int rand = 
+    }
+}
