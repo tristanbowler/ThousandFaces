6908c1b9e30ff0ee56e2c6c37303115a190e2bc2
Tristan Bowler <tristan.bowler@gmail.com>
2020-11-19 20:29:08 -0700

    Giant worms

---
+++ ThousandFacesPrototype/Assets/InfinityPBR/_InfinityPBR - Giant Worm/Scripts/SFB_DemoGiantWorm.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFB_DemoGiantWorm : MonoBehaviour {
+
+	public ParticleSystem[] magicSpell;
+	public ParticleSystem[] groundParticles;
+
+	public void StartMagicSpell(){
+		for (int i = 0; i <magicSpell.Length; i++){
+			magicSpell[i].Play();
+		}
+	}
+
+	public void StopMagicSpell(){
+		for (int i = 0; i <magicSpell.Length; i++){
+			magicSpell[i].Stop();
+		}
+	}
+
+	public void StartGround(){
+		for (int i = 0; i <groundParticles.Length; i++){
+			groundParticles[i].Play();
+		}
+	}
+
+	public void StopGround(){
+		for (int i = 0; i <groundParticles.Length; i++){
+			groundParticles[i].Stop();
+		}
+	}
+}
