fb4aed976a5a6aa002779b385372af2f08776012
Tristan Bowler <tristan.bowler@gmail.com>
2020-11-19 20:37:51 -0700

    horsey nay nay

---
+++ ThousandFacesPrototype/Assets/Malbers Animations/Common/Scripts/Animal Controller/AI Brain/Tasks/InvokeEventTask.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace MalbersAnimations.Controller.AI
+{
+    [CreateAssetMenu(menuName = "Malbers Animations/Pluggable AI/Tasks/Invoke Event")]
+    public class InvokeEventTask : MTask
+    {
+        [Space]
+        public UnityEvent Raise = new UnityEvent();
+        
+        public override void StartTask(MAnimalBrain brain, int index)
+        {
+            Raise.Invoke();
+        }
+
+        void Reset() { Description = "Raise the Event when the Task start. Use this only for Scriptable Assets"; }
+    }
+}
---
+++ ThousandFacesPrototype/Assets/Malbers Animations/Common/Scripts/Behaviors/IsKinematicB.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace MalbersAnimations
+{
+    public class IsKinematicB : StateMachineBehaviour
+    {
+        public enum OnEnterOnExit { OnEnter, OnExit, OnEnterOnExit}
+        public OnEnterOnExit SetKinematic = OnEnterOnExit.OnEnterOnExit;
+
+        [Tooltip("Changes the Kinematic property of the RigidBody On Enter/OnExit")]
+        [Hide("onenterexit",true,true)]
+        public bool isKinematic = true;
+
+        Rigidbody rb;
+        override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            rb = animator.GetComponent<Rigidbody>();
+            if (SetKinematic == OnEnterOnExit.OnEnter)
+            {
+                rb.isKinematic = isKinematic;
+            }
+            else if (SetKinematic == OnEnterOnExit.OnEnterOnExit)
+            {
+                rb.isKinematic = true;
+            }
+        }
+
+        override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (SetKinematic == OnEnterOnExit.OnExit)
+            {
+                rb.isKinematic = isKinematic;
+            }
+            else if (SetKinematic == OnEnterOnExit.OnEnterOnExit)
+            {
+                rb.isKinematic = false;
+            }
+        }
+
+        [HideInInspector] public bool onenterexit;
+        private void OnValidate()
+        {
+            onenterexit = SetKinematic == OnEnterOnExit.OnEnterOnExit;
+        }
+    }
+}
