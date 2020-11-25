a13a87c54b7a07905bfddab7bc9def321414a5bd
Tristan Bowler <tristan.bowler@gmail.com>
2020-11-24 17:36:26 -0700

    mimics

---
+++ ThousandFacesPrototype/Assets/InfinityPBR/_InfinityPBR - Mimics & Chests/Scripts/SFBDemo_Mimics.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SFBDemo_Mimics : MonoBehaviour {
+
+	Animator animator;											// Reference to animator component
+
+	public ParticleSystem[] castWarmup;							// Array of warmup particles
+	public ParticleSystem[] castSpell;							// Array of spell particles
+
+	public string locomotionName = "locomotion";				// Name of locomotion variable in Animator
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();					// Assign reference
+		if (!animator) {										// If there is no animator
+			// Show an error
+			Debug.LogError ("Warning:  No Animator is found on " + gameObject.name + ", but the Demo Script requires it!");
+		}
+	}
+
+	/// <summary>
+	/// Updates the locomotion value in the Animator
+	/// </summary>
+	/// <param name="newValue">New value.</param>
+	public void UpdateLocomotion(float newValue){
+		if (animator) {											// If animator is assigned
+			animator.SetFloat (locomotionName, newValue);		// Update the value
+		}
+	}
+
+	/// <summary>
+	/// Starts the cast particles
+	/// </summary>
+	public void StartWarmup()
+	{
+		//Debug.Log("Start Warmup");
+		for (int i = 0; i < castWarmup.Length; i++){			// For each castWarmup particle
+			castWarmup [i].Play ();								// Play()
+		}
+	}
+
+	/// <summary>
+	/// Stops the Warmup and starts the Cast particles
+	/// </summary>
+	public void StartCast()
+	{
+		//Debug.Log("Start Cast");
+		for (int i = 0; i < castWarmup.Length; i++){			// For each castWarmup particle
+			castWarmup [i].Stop ();								// Stop()
+		}
+		for (int i = 0; i < castSpell.Length; i++){				// For each castSpell particle
+			castSpell [i].Play ();								// Play()
+		}
+	}
+
+	/// <summary>
+	/// Stops the Cast portion
+	/// </summary>
+	public void StopCast(){
+		for (int i = 0; i < castSpell.Length; i++){				// For each castSpell particle
+			castSpell [i].Stop ();								// Stop()
+		}
+	}
+}
